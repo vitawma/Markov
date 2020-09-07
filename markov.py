0c9be5125346f6eaf45682ed1e43fed4f7fd6422
Michael <vitao.moraes@gmail.com>
2020-09-07 14:35:35 -0300

    melhorando o modulo

--- markov.py
+++ markov.py
@@ -5,69 +5,6 @@ https://www.rose-hulman.edu/class/csse/csse221/200910/Projects/Markov/markov.htm
 
 import random
 
-def some_stuff():
-
-	def prod(gen):
-		try:
-			return next(gen)*prod(gen)
-		except StopIteration:
-			return 1
-
-	x = {0: Perc(0.50),
-		 1: Perc(0.50)}
-
-	y = {1: Perc(1/3),
-		 2: Perc(1/3),
-		 3: Perc(1/3)}
-
-	xy = {0: Perc(1/2),
-		  1: Perc(1/6),
-		  2: Perc(1/6),
-		  3: Perc(1/6)}
-
-	z = {1: Perc(0.9),
-		 10: Perc(0.1)}
-
-	def mean_randdiscr_variable(x):
-		return sum(k*x[k] for k in x)
-
-	def mean_product_of_randdiscr_variable(*my_vars):
-		import itertools
-		print(type(my_vars))
-		print(my_vars)
-		cart_product = itertools.product(*my_vars)
-		my_sum = 0
-		return sum(prod(c[i]*my_vars[i][c[i]]
-						for i in range(len(my_vars)))
-				   for c in cart_product)
-
-	# print(mean_randdiscr_variable(x))
-	# print(mean_randdiscr_variable(y))
-	# print(mean_product_of_randdiscr_variable(x,y,z))
-
-	def fact(n):
-		return 1 if n <= 1 else n*fact(n-1)
-
-	class Perc(float):
-		def __str__(self):
-			return '%.2f%%' % (float(self) * 100)
-
-		def __repr__(self):
-			return self.__str__()
-
-	def chance_greater_or_eq(alpha, p, n):
-		'''
-		what is the chance of obtaining the event E,
-		whose probability is alpha, at least p out of n times?
-		'''
-		return Perc( fact(n)*sum( (alpha**k * (1-alpha)**(n-k) / fact(k) / fact(n-k)) for k in range (p,n+1) ) )
-
-	# alpha = 67.11 / 100
-	# print(chance_greater_or_eq(alpha, 3, 6))
-	# print(chance_greater_or_eq(alpha, 4, 6))
-	# print(chance_greater_or_eq(alpha, 5, 6))
-	# print(chance_greater_or_eq(alpha, 6, 6))
-
 class Suffix:
 	def __init__(self,obj):
 		self.obj = obj
@@ -87,14 +24,10 @@ class Suffix:
 
 class Prefix(tuple):
 	def __new__(cls,my_tuple):
-		'''about __new__ and super() methods:
-		https://www.geeksforgeeks.org/__new__-in-python/
-		https://www.tutorialsteacher.com/python/magic-methods-in-python
-		https://www.programiz.com/python-programming/methods/built-in/super
-		'''
 		my_object = super().__new__(cls, my_tuple)
 		my_object.suffixes = []
 		my_object.prob_table = []
+		my_object.sum = 0
 		return my_object
 
 	def add_suffix(self,suffix):
@@ -105,11 +38,11 @@ class Prefix(tuple):
 		self.suffixes.append(Suffix(suffix))
 
 	def create_prob_table(self):
-		total = sum(s.mult for s in self.suffixes)
-		self.prob_table.append(self.suffixes[0].mult/total)
+		self.sum = sum(s.mult for s in self.suffixes)
+		self.prob_table.append(self.suffixes[0].mult/self.sum)
 		for i in range (1,len(self.suffixes)):
 			self.prob_table.append(self.prob_table[-1]+
-								   self.suffixes[i].mult/total)
+								   self.suffixes[i].mult/self.sum)
 
 	def print_prob_table(self):
 		if self.prob_table == []:
@@ -124,21 +57,20 @@ class Prefix(tuple):
 		r = random.random()
 		for i in range(len(self.prob_table)):
 			if r < self.prob_table[i]:
-				if no_non_word and self.suffixes[i].obj == Non_Word():
+				if no_non_word and self.suffixes[i].obj == NON_WORD:
 					self.give(no_non_word=True)
 				else:
 					return self.suffixes[i].obj
 
 
 	def __eq__(self,other):
-		# this seems to work...
-		return super(tuple,self).__eq__(other)
-		# if len(self) != len(other):
-		# 	return False
-		# for i in range(len(self)):
-		# 	if self[i] != other[i]:
-		# 		return False
-		# return True
+		if len(other) != len(self):
+			return False
+		for i in range(len(self)):
+			if self[i] != other[i]:
+				return False
+		return True
+		# return super(tuple,self).__eq__(other)
 
 	def __repr__(self):
 		s =  'Prefix = ('
@@ -154,90 +86,144 @@ class Prefix(tuple):
 		return s
 
 class Non_Word:
-	def __repr__(self):
-		return __str__(self)
 	def __str__(self):
 		return 'NON_WORD'
+	def __repr__(self):
+		return 'NON_WORD'
+	def __eq__(self,other):
+		return type(other) == Non_Word
+
+NON_WORD = Non_Word()
 
 class Markov(list):
 	def __init__(self,n):
 		self.n = n
 
 	def get_prefix(self,my_prefix):
+		my_prefix = to_tuple(my_prefix)
 		return next((x for x in self if x == my_prefix), None)
 
 	def get_random_prefix(self):
 		return random.choice(self)
 
-	def build_chain(self,my_list):
+	def build_chain(self,my_list,list_of_lists=False):
 		if len(my_list) < self.n + 1:
 			print('Impossible to build chain, too little data')
 
-		for i in range(self.n):
-			my_prefix = Prefix((Non_Word(),)*(self.n-1-i) +
-							   tuple(my_list[0:i+1]))
-			my_prefix.add_suffix(my_list[i+1])
-			
-			self.append(my_prefix)
+		if not list_of_lists:
+			my_list = [my_list]
+
+		for p in my_list:
+			for i in range(min(self.n,len(p))):
+				my_prefix = Prefix((NON_WORD,)*(self.n-i) +
+								   tuple(p[0:i]))
+
+				if my_prefix in self:
+					my_prefix = self.get_prefix(my_prefix)
+				else:
+					self.append(my_prefix)
+
+				if i < len(p):
+					my_prefix.add_suffix(p[i])
+				else:
+					my_prefix.add_suffix(NON_WORD)
+
+			'''fazer a mesma coisa para o final da palavra'''
+			'''o único sufixo possível deve ser NON_WORD'''
+			for i in range(1,min(self.n,len(p))+1):
+				my_prefix = Prefix(tuple(p[-i:]) + (NON_WORD,)*(self.n-i))
+				if my_prefix in self:
+					my_prefix = self.get_prefix(my_prefix)
+				else:
+					self.append(my_prefix)
+				my_prefix.add_suffix(NON_WORD)
 
 		progress = 0
-		for i in range(1,len(my_list)-self.n+1):
-			if i / len(my_list) > progress + 0.1:
-				print("%2d%%" % (i / len(my_list)*100))
+		count = 0
+		for p in my_list:
+			if count / len(my_list) > progress + 0.1:
+				print("%2d%%" % ((progress+0.1)*100))
 				progress += 0.1
-			my_prefix = tuple(my_list[i:i+self.n]) #Prefix(tuple(my_list[i:i+self.n]))
-			# https://stackoverflow.com/questions/7125467/find-object-in-list-that-has-attribute-equal-to-some-value-that-meets-any-condi
-			my_obj = self.get_prefix(my_prefix)
-			if my_obj != None:
-				if i + self.n < len(my_list):
-					my_obj.add_suffix(my_list[i+self.n])
+
+			for i in range(1,len(p)-self.n+1):
+				
+				my_prefix = to_tuple(p[i:i+self.n])
+				
+				my_obj = self.get_prefix(my_prefix)
+				if my_obj != None:
+					if i + self.n < len(p):
+						my_obj.add_suffix(p[i+self.n])
+					else:
+						my_obj.add_suffix(NON_WORD)
 				else:
-					my_obj.add_suffix(Non_Word())
-			else:
-				self.append(Prefix(my_prefix))
-				if i + self.n < len(my_list):
-					self[-1].add_suffix(my_list[i+self.n])
-				else:
-					self[-1].add_suffix(Non_Word())
+					self.append(Prefix(my_prefix))
+					if i + self.n < len(p):
+						self[-1].add_suffix(p[i+self.n])
+					else:
+						self[-1].add_suffix(NON_WORD)
+
+			count += 1
 
 	def __repr__(self):
 		return __str__(self)
 
-	def __str__(self,long_print=False):
+	def __str__(self,long_print=True):
 		s = 'Markov chain with %d prefixes, of which:\n' % len(self)
 		if long_print:
 			for prefix in self:
 				s += str(prefix)
 		return s
 
-
+def to_tuple(x):
+	try:
+		return(tuple(x))
+	except TypeError:
+		return(x,)
 
 def test_1():
 	my_str = '''
-In statistics, a contingency table (also known as a cross tabulation or crosstab) is a type of table in a matrix format that displays the (multivariate) frequency distribution of the variables. They are heavily used in survey research, business intelligence, engineering and scientific research. They provide a basic picture of the interrelation between two variables and can help find interactions between them. The term contingency table was first used by Karl Pearson in "On the Theory of Contingency and Its Relation to Association and Normal Correlation",[1] part of the Drapers' Company Research Memoirs Biometric Series I published in 1904.
-A crucial problem of multivariate statistics is finding the (direct-)dependence structure underlying the variables contained in high-dimensional contingency tables. If some of the conditional independences are revealed, then even the storage of the data can be done in a smarter way (see Lauritzen (2002)). In order to do this one can use information theory concepts, which gain the information only from the distribution of probability, which can be expressed easily from the contingency table by the relative frequencies.
-A pivot table is a way to create contingency tables using spreadsheet software. 
-The numbers of the males, females, and right- and left-handed individuals are called marginal totals. The grand total (the total number of individuals represented in the contingency table) is the number in the bottom right corner.
-The table allows users to see at a glance that the proportion of men who are right handed is about the same as the proportion of women who are right handed although the proportions are not identical. The strength of the association can be measured by the odds ratio, and the population odds ratio estimated by the sample odds ratio. The significance of the difference between the two proportions can be assessed with a variety of statistical tests including Pearson's chi-squared test, the G-test, Fisher's exact test, Boschloo's test and Barnard's test, provided the entries in the table represent individuals randomly sampled from the population about which conclusions are to be drawn. If the proportions of individuals in the different columns vary significantly between rows (or vice versa), it is said that there is a contingency between the two variables. In other words, the two variables are not independent. If there is no contingency, it is said that the two variables are independent.
-The example above is the simplest kind of contingency table, a table in which each variable has only two levels; this is called a 2 × 2 contingency table. In principle, any number of rows and columns may be used. There may also be more than two variables, but higher order contingency tables are difficult to represent visually. The relation between ordinal variables, or between ordinal and categorical variables, may also be represented in contingency tables, although such a practice is rare. For more on the use of a contingency table for the relation between two ordinal variables, see Goodman and Kruskal's gamma. 
-'''
-	my_str = open('bible.txt','r').read()
+In statistics a contingency table (also known as a cross tabulation or crosstab) is a type of table'''# in a matrix format that displays the (multivariate) frequency distribution of the variables. They are heavily used in survey research, business intelligence, engineering and scientific research. They provide a basic picture of the interrelation between two variables and can help find interactions between them. The term contingency table was first used by Karl Pearson in "On the Theory of Contingency and Its Relation to Association and Normal Correlation",[1] part of the Drapers' Company Research Memoirs Biometric Series I published in 1904.
+# A crucial problem of multivariate statistics is finding the (direct-)dependence structure underlying the variables contained in high-dimensional contingency tables. If some of the conditional independences are revealed, then even the storage of the data can be done in a smarter way (see Lauritzen (2002)). In order to do this one can use information theory concepts, which gain the information only from the distribution of probability, which can be expressed easily from the contingency table by the relative frequencies.
+# A pivot table is a way to create contingency tables using spreadsheet software. 
+# The numbers of the males, females, and right- and left-handed individuals are called marginal totals. The grand total (the total number of individuals represented in the contingency table) is the number in the bottom right corner.
+# The table allows users to see at a glance that the proportion of men who are right handed is about the same as the proportion of women who are right handed although the proportions are not identical. The strength of the association can be measured by the odds ratio, and the population odds ratio estimated by the sample odds ratio. The significance of the difference between the two proportions can be assessed with a variety of statistical tests including Pearson's chi-squared test, the G-test, Fisher's exact test, Boschloo's test and Barnard's test, provided the entries in the table represent individuals randomly sampled from the population about which conclusions are to be drawn. If the proportions of individuals in the different columns vary significantly between rows (or vice versa), it is said that there is a contingency between the two variables. In other words, the two variables are not independent. If there is no contingency, it is said that the two variables are independent.
+# The example above is the simplest kind of contingency table, a table in which each variable has only two levels; this is called a 2 × 2 contingency table. In principle, any number of rows and columns may be used. There may also be more than two variables, but higher order contingency tables are difficult to represent visually. The relation between ordinal variables, or between ordinal and categorical variables, may also be represented in contingency tables, although such a practice is rare. For more on the use of a contingency table for the relation between two ordinal variables, see Goodman and Kruskal's gamma. 
+# '''
+	#my_str = open('bible.txt','r').read()
 	my_str = my_str.replace('\n',' ')
-	my_list = list(my_str.lower())
+	my_str = my_str.replace('(',' ')
+	my_str = my_str.replace(')',' ')
+	my_str = my_str.replace(',',' ')
+	my_list = my_str.lower().split()
+	print(my_list)
 
-	n = 4
+	n = 2
 	a = Markov(n)
-	a.build_chain(my_list)
+	a.build_chain(my_list,list_of_lists=True)
 	print(a)
-	r = a.get_prefix(tuple('the '))
-	print(r)
+	r = a.get_prefix(to_tuple('i'))
+	
 	print('-'*50)
+	# print(r)
+	print('-'*50)
+	# r = a.get_prefix(to_tuple(NON_WORD))
+	# print(r)
 	prefix = a.get_random_prefix()
-	for i in range(1000):
-		s = prefix.give(no_non_word=True)
-		print(s, end='')
-		prefix = a.get_prefix(prefix[1:] + tuple(s))
+	print(prefix)
+	for i in range(100):
+		s = prefix.give(no_non_word=False)
+		if s==NON_WORD:
+			print(' ', end='')
+		else:
+			print(s, end='')
+		novo = prefix[1:] + to_tuple(s)
+		print(novo)
+		print(prefix[1:])
+		print(to_tuple(s))
+		prefix = a.get_prefix(prefix[1:] + to_tuple(s))
+		print(prefix)
 
-import cProfile
-cProfile.run('test_1()')
+# import cProfile
+# cProfile.run('test_1()')
 
+test_1()
